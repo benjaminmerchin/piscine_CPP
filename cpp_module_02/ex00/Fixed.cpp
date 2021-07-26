90246c6b418c7005cd24766fb3688f3b7c0b8f3e
benjaminmerchin <benjaminmerchin@gmail.com>
2021-07-26 17:50:38 +0200

    oui

---
+++ cpp_module_02/ex00/Fixed.cpp
@@ -0,0 +1,35 @@
+#include "Fixed.hpp"
+
+Fixed::Fixed(void) : _value_fixed_point(0) {
+	std::cout << "Default constructor called" << std::endl;
+}
+
+Fixed::Fixed(Fixed const & src) {
+	std::cout << "Copy constructor called" << std::endl;
+	*this = src;
+}
+
+Fixed::~Fixed(void) {
+	std::cout << "Destructor called" << std::endl;
+}
+
+Fixed & Fixed::operator=(Fixed const & rhs) {
+	std::cout << "Assignation operator called" << std::endl;
+	this->_value_fixed_point = rhs.getRawBits();
+	return *this;
+}
+
+int Fixed::getRawBits(void) const {
+	std::cout << "getRawBits member function called" << std::endl;
+	return this->_value_fixed_point;
+}
+
+void Fixed::setRawBits(int const raw){
+	std::cout << "setRawBits member function called" << std::endl;
+	this->_value_fixed_point = raw;
+}
+
+std::ostream & operator<<(std::ostream & o, Fixed const & rhs) {
+	o << rhs.getRawBits();
+	return o;
+}
---
+++ cpp_module_02/ex00/Fixed.hpp
@@ -0,0 +1,24 @@
+#ifndef KAREN_HPP
+# define KAREN_HPP
+
+# include <iostream>
+
+class Fixed {
+public:
+	Fixed(void);
+	Fixed(Fixed const & src);
+	~Fixed(void);
+
+	Fixed & operator=(Fixed const & rhs);
+
+	int getRawBits(void) const;
+	void setRawBits(int const raw);
+
+private:
+	int _value_fixed_point;
+	static const int _number_bits = 8;
+};
+
+std::ostream & operator<<(std::ostream & o, Fixed const & i);
+
+#endif
