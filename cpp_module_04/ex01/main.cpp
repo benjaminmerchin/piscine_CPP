667b149b4c34903561b6471481206a7dbdde0da2
benjaminmerchin <benjaminmerchin@gmail.com>
2021-08-06 18:41:36 +0200

    oui

--- cpp_module_04/ex01/main.cpp
+++ cpp_module_04/ex01/main.cpp
@@ -7,23 +7,27 @@
 
 int main()
 {
-	const Cat *i = new Cat();
-	const Animal* j = new Dog();
-	Cat k;
-	std::cout << std::endl;
+	{
+		const Animal* i = new Cat();
+		const Animal* j = new Dog();
+		Cat k;
+		std::cout << std::endl;
 
-	k = *i;
-	delete i;
-	k.makeSound();
-	j->makeSound();
-	std::cout << std::endl;
+		(Animal)k = *i;
+		std::cout << std::endl;
 
-	delete j;
+		delete i;
+		k.makeSound();
+		j->makeSound();
+		std::cout << std::endl;
+
+		delete j;
+	}
 	std::cout << std::endl;
 
 	std::cout << "/*===============================*/\n\n";
 
-	/*{
+	{
 		Dog a;
 		Dog b;
 		Dog c(a);
@@ -34,11 +38,11 @@ int main()
 		std::cout << b.getBrain() << std::endl;
 		std::cout << c.getBrain() << std::endl;
 	}
-	std::cout << std::endl;*/
+	std::cout << std::endl;
 
 	std::cout << "/*===============================*/\n\n";
 
-	/*int size = 8;
+	int size = 8;
 	Animal* Snoopy[size];
 	for (int i=0; i < size; i++) {
 		if (i%2==0)
@@ -54,5 +58,4 @@ int main()
 
 	for (int i=0; i < size; i++)
 		delete Snoopy[i];
-	*/
 }
