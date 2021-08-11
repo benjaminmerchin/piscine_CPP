decddb452c40c8cd0b69e89bc4d468248f7457e1
benjaminmerchin <benjaminmerchin@gmail.com>
2021-08-11 15:33:23 +0200

    oui

--- cpp_module_04/ex03/main.cpp
+++ cpp_module_04/ex03/main.cpp
@@ -16,10 +16,13 @@ int main()
 	me->equip(tmp);
 	tmp = src->createMateria("cure");
 	me->equip(tmp);
+	tmp = src->createMateria("random");
+	me->equip(tmp);
 	ICharacter* bob = new Character("bob");
 	std::cout << "===========================================\n";
 	me->use(0, *bob);
 	me->use(1, *bob);
+	me->use(2, *bob);
 	std::cout << "===========================================\n";
 	delete bob;
 	delete me;
