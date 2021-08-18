15ba0408c64d569b5b678e4a5c4c7bdaf10a6bc7
benjaminmerchin <benjaminmerchin@gmail.com>
2021-08-18 22:37:12 +0200

    oui

---
+++ cpp_module_06/ex01/main.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+
+class Data {
+public:
+	int n;
+};
+
+//retournera le paramètre sous la forme d’un nombre entier
+uintptr_t serialize(Data* ptr) {
+	return reinterpret_cast<uintptr_t>(ptr);
+}
+
+//renvoie les données brutes que vous avez créées
+Data* deserialize(uintptr_t raw) {
+	return reinterpret_cast<Data*>(raw);
+}
+
+int main(void) {
+	Data * addr = new Data;
+	addr->n = 50;
+	std::cout << "Address_initial:    " << addr << "    Value_Stored: " << addr->n << std::endl;
+	uintptr_t addr_temp = serialize(addr);
+	std::cout << "Address_serialized: " << addr_temp << std::endl;
+	Data * addr_recovered = deserialize(addr_temp);
+	std::cout << "Address_recovered:  " << addr_recovered << "    Value_Stored: " << addr_recovered->n << std::endl;
+	return 0;
+}
