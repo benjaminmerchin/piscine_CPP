b83d4556bbd605abb975a199b46b4042bd002cf5
benjaminmerchin <benjaminmerchin@gmail.com>
2021-08-25 16:30:24 +0200

    oui

--- cpp_module_08/ex00/main.cpp
+++ cpp_module_08/ex00/main.cpp
@@ -2,48 +2,34 @@
 
 //https://www.cplusplus.com/reference/algorithm/find/
 
-int main(void) {
-	std::vector<int> myvector;
+template <typename T>
+void testor() {
+	T myvector;
 	myvector.push_back(1);
 	myvector.push_back(2);
 	myvector.push_back(9);
 	myvector.push_back(3);
 	myvector.push_back(5);
-	std::vector<int>::iterator it;
+	typename T::iterator it;
 
 	it = easyfind(myvector, 10);
 	if (it != myvector.end())
-		std::cout << "Element found in myvector: " << *it << '\n';
+		std::cout << "Element found: " << *it << '\n';
 	else
-		std::cout << "Element not found in myvector\n";
+		std::cout << "Element not found\n";
 
-	it = easyfind(myvector, 9);
+	it = easyfind(myvector, 5);
 	if (it != myvector.end())
-		std::cout << "Element found in myvector: " << *it << '\n';
+		std::cout << "Element found: " << *it << '\n';
 	else
-		std::cout << "Element not found in myvector\n";
+		std::cout << "Element not found\n";
+}
 
-	std::cout << "============================\n";
-
-	std::list<int> mylist;
-	mylist.push_back(1);
-	mylist.push_back(2);
-	mylist.push_back(9);
-	mylist.push_back(3);
-	mylist.push_back(5);
-	std::list<int>::iterator lit;
-
-	lit = easyfind(mylist, 10);
-	if (lit != mylist.end())
-		std::cout << "Element found in mylist: " << *lit << '\n';
-	else
-		std::cout << "Element not found in mylist\n";
-
-	lit = easyfind(mylist, 9);
-	if (lit != mylist.end())
-		std::cout << "Element found in mylist: " << *lit << '\n';
-	else
-		std::cout << "Element not found in mylist\n";
-
+int main(void) {
+	testor<std::vector<int> >();
+	std::cout << "=================\n";
+	testor<std::list<unsigned int> >();
+	std::cout << "=================\n";
+	testor<std::list<float> >();
 	return 0;
 } 
--- cpp_module_08/ex01/span.hpp
+++ cpp_module_08/ex01/span.hpp
@@ -2,7 +2,7 @@
 #define SPAN_HPP
 
 #include <iostream>
-#include <list>
+#include <vector>
 
 class Span {
 public:
@@ -13,6 +13,17 @@ public:
 	Span & operator=(Span const & src);
 
 private:
+	std::vector<int> _content;
 };
 
 #endif
+
+//regarder vite fait les containers existants
+//pour vector, regarder les fonctions existantes
+//rajouter juste la fonction getnumber
+
+//pour le 02, faire une class qui herite d'un autre container
+//faire heriter directement dans la deff du container et dans les 2 constructeurs
+//faire iterator et const_iterator begin et end pour le 02
+//utiliser le container c qui est herite par le container stack
+//tout peut se faire dans le .hpp et on peut laisser le .cpp vide
