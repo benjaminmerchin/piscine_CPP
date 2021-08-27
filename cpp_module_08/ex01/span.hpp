608c46ec3f71b6c6f658217ed376c0b6a1b7bef7
benjaminmerchin <benjaminmerchin@gmail.com>
2021-08-27 11:34:47 +0200

    oui

--- cpp_module_08/ex01/span.hpp
+++ cpp_module_08/ex01/span.hpp
@@ -39,8 +39,3 @@ private:
 };
 
 #endif
-//8+25+0+7+4+17+3+6+0+2+9+12+3+9+1 = 7.06 personnes qui ont fait des correc
-//7+13+14+9+5+11+1-3+2+6+0+6+9+4+1 = 5.66 personnes de ma promo top 51->65
-//3+2+5+1+25+6+1+6+4+3+6+18+5+10+1 = 6.4 personnes de ma promo top 66->80
-//6+4+0+5+4+17+0+5+6+5+1+11+9+5+9+8+6+0+6+7 = 5.7 personnes de ma promo top 81->100
-//3+4+6+15+0+3+8+2+11+1+1+11+4+6+3+6+3+3+3+5 = 4.9 personnes de ma promo top 101->120
